80d687c03154c1303f23cd67b6f519f279f344ac
Linus Silbernagel <linussilbernagel@gmail.com>
2023-12-12 18:35:52 -0500

    possibly completed simulator_interface

--- Simulation/Simulator_Python/simulator_interface_ai.cpp
+++ Simulation/Simulator_Python/simulator_interface_ai.cpp
@@ -14,37 +14,45 @@
 #include <stdexcept>
 #include <iterator>
 
+// #include "simulator_ai.cpp"
+
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/program_options.hpp>
+
+
 using namespace std;
 
-class Element {
-private:
-    string __act;
-    string __inh;
-public:
-    Element(string act, string inh) : __act(act), __inh(inh) {}
-    string get_act() { return __act; }
-    string get_inh() { return __inh; }
-};
+namespace po = boost::program_options;
 
-class Simulator {
-private:
-    string inputFilename;
-    map<string, Element> elements;
-public:
-    Simulator(string inputFilename) : inputFilename(inputFilename) {
-        // Read input file and initialize elements
-    }
-    map<string, Element> get_elements() { return elements; }
-    void create_rules(string outputFilename, int scenario) {
-        // Create rules and write to output file
-    }
-    void create_truth_tables(string outputBaseFilename, int scenario) {
-        // Create truth tables and write to output files
-    }
-    void run_simulation(string simScheme, int runs, int steps, string outputFilename, int scenario, int outMode, bool normalize, bool randomizeEachRun, string eventTracesFile, bool progressReport) {
-        // Run simulation and write to output file
-    }
-};
+// class Element {
+// private:
+//     string __act;
+//     string __inh;
+// public:
+//     Element(string act, string inh) : __act(act), __inh(inh) {}
+//     string get_act() { return __act; }
+//     string get_inh() { return __inh; }
+// };
+
+// class Simulator {
+// private:
+//     string inputFilename;
+//     map<string, Element> elements;
+// public:
+//     Simulator(string inputFilename) : inputFilename(inputFilename) {
+//         // Read input file and initialize elements
+//     }
+//     map<string, Element> get_elements() { return elements; }
+//     void create_rules(string outputFilename, int scenario) {
+//         // Create rules and write to output file
+//     }
+//     void create_truth_tables(string outputBaseFilename, int scenario) {
+//         // Create truth tables and write to output files
+//     }
+//     void run_simulation(string simScheme, int runs, int steps, string outputFilename, int scenario, int outMode, bool normalize, bool randomizeEachRun, string eventTracesFile, bool progressReport) {
+//         // Run simulation and write to output file
+//     }
+// };
 
 std::string join(std::vector<std::string> const &strings, std::string delim)
 {
@@ -166,10 +174,11 @@ void calculate_trace_difference(vector<string> inputFilenames, string outputFile
     string line;
     // TODO: Check if leading/trailing whitespace matters
     while (getline(first, line)) {
+        boost::algorithm::trim(line);
         first_trace_data.push_back(line);
     }
     bool transpose_format = false;
-    if (first_trace_data[0][0] == '#') { //TODO: check if \t or #
+    if (first_trace_data[0][0] == '#') { 
         transpose_format = true;
     }
 
@@ -182,15 +191,16 @@ void calculate_trace_difference(vector<string> inputFilenames, string outputFile
         ofstream output_file(outputFilename.substr(0, outputFilename.find_last_of('.')) + output_index + ".txt");
         ifstream this_file(this_filename);
         vector<string> this_trace_data;
-        // TODO: Check if whitespace matters
+        // TODO: Check if whitespace matters, using boost trim
         while (getline(this_file, line)) {
+            boost::algorithm::trim(line);
             this_trace_data.push_back(line);
         }
         for (int content_index = 0; content_index < this_trace_data.size(); content_index++) {
             string current_trace = this_trace_data[content_index];
             if (current_trace.find("Frequency") != string::npos || current_trace.find("Run") != string::npos) {
                 output_file << current_trace << endl;
-            } else if (current_trace[0] == '#') { // TODO: Check if \t or #, also should use find not [0]
+            } else if (current_trace.find('#') != string::npos) { 
                 if (current_trace != first_trace_data[content_index]) {
                     throw invalid_argument("Element names in trace files do not match");
                 }
@@ -231,6 +241,8 @@ void calculate_trace_difference(vector<string> inputFilenames, string outputFile
     }
 }
 
+
+// Function incorrect here and in python
 void concatenate_traces(vector<string> inputFilenames, string outputFilename) {
     if (inputFilenames.size() == 1) {
         throw invalid_argument("Cannot concatenate with only one input file");
@@ -268,82 +280,159 @@ void concatenate_traces(vector<string> inputFilenames, string outputFilename) {
     }
 }
 
-map<string, vector<vector<int>>> trace_distributions(string model_file, string traces_file, int time_units, int scale_factor = 1, bool normalize = false) {
-    map<string, vector<vector<int>>> distributions;
-    map<string, vector<vector<int>>> traces = get_traces(traces_file);
-    for (int i = 0; i < calculate_toggle_steps(model_file, range(time_units + 1), 1).size(); i++) {
-        for (auto el : traces) {
-            vector<vector<int>> M;
-            for (auto v : el.second) {
-                M.push_back(v);
-            }
-            distributions[el.first].push_back(M[calculate_toggle_steps(model_file, range(time_units + 1), 1)[i]]);
-            if (normalize) {
-                for (int j = 0; j < distributions[el.first][i].size(); j++) {
-                    distributions[el.first][i][j] /= traces[el.first].size() - 1;
-                }
-            }
-        }
-    }
-    return distributions;
-}
+// Utilizes viz, not worth it
+// Never gets called
+// map<string, vector<vector<int>>> trace_distributions(string model_file, string traces_file, int time_units, int scale_factor = 1, bool normalize = false) {
+//     map<string, vector<vector<int>>> distributions;
+//     map<string, vector<vector<int>>> traces = get_traces(traces_file);
+//     for (int i = 0; i < calculate_toggle_steps(model_file, range(time_units + 1), 1).size(); i++) {
+//         for (auto el : traces) {
+//             vector<vector<int>> M;
+//             for (auto v : el.second) {
+//                 M.push_back(v);
+//             }
+//             distributions[el.first].push_back(M[calculate_toggle_steps(model_file, range(time_units + 1), 1)[i]]);
+//             if (normalize) {
+//                 for (int j = 0; j < distributions[el.first][i].size(); j++) {
+//                     distributions[el.first][i][j] /= traces[el.first].size() - 1;
+//                 }
+//             }
+//         }
+//     }
+//     return distributions;
+// }
 
 void get_input_args() {
     // Parse command line arguments
 }
 
-int main() {
-    get_input_args();
-    // Set up logging
+int main(int argc, char *argv[]) {
+
+    // Parse command line arguments
+    string input_filename;
+    string output_filename;
+    string sim_scheme;
+    int runs;
+    int steps;
+    int output_format;
+    string event_traces_file;
+    bool normalize_output;
+    string scenarios;
+    bool difference;
+    bool concatenate;
+    bool randomize_each_run;
+    bool timed;
+    po::options_description desc("Run a simulation");
+    desc.add_options()
+        ("input_filename", po::value<string>(& input_filename), 
+            "path and name of the input file to use for the simulation")
+        ("output_filename", po::value<string>(& output_filename),
+            "path and name to use for the output file")
+        ("sim_scheme", po::value<string>(& sim_scheme)->default_value("ra"),
+            "simulation scheme")
+        ("runs", po::value<int>(& runs)->default_value(100),
+            "number of simulation runs (repetitions)")
+        ("steps", po::value<int>(& steps)->default_value(1000),
+            "number of simulation steps (duration)")
+        ("output_format", po::value<int>(& output_format)->default_value(0),
+            "format of trace file output")
+        ("event_traces_file", po::value<string>(& event_traces_file)->default_value(""), 
+            "path and name to use for the event traces file")
+        ("normalize_output", po::value<bool>(& normalize_output)->default_value(false), 
+            "in traces file, normalize values to the range [0,1]")
+        ("scenarios", po::value<string>(& scenarios)->default_value("0"), 
+            "scenario index specifying which Initial column to use \nor comma-sparated list of scenarios indices \n")
+        ("difference", po::value<bool>(& difference)->default_value(false), 
+            "calculate the difference between traces for the first scenario \nand traces from any other scenarios")
+        ("concatenate", po::value<bool>(& concatenate)->default_value(false), 
+            "concatenate trace files for all scenarios")
+        ("randomize_each_run", po::value<bool>(& randomize_each_run)->default_value(false), 
+            "For sequential schemes, randomize initial values in each run.")
+        ("timed", po::value<bool>(& timed)->default_value(false), 
+            "log the total simulation time");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    // Check some argument formats
+    if (concatenate && output_format != 2)
+        throw invalid_argument("Concatenation only supported for output_format=2");
+    if (difference && scenarios.length() < 2)
+        throw invalid_argument("Need at least two scenarios to calculate the difference");
+    if (concatenate && scenarios.length() < 2)
+        throw invalid_argument("Need at least two scenarios to concatenate trace files");
+    if (sim_scheme == "fixed_updates" && event_traces_file == "")
+        throw invalid_argument("Need to specify the event_traces file");
+    
+
+    // Set default trace file format
     if (output_format == 0) {
         if (sim_scheme == "ra" || sim_scheme == "round" || sim_scheme == "ra_multi") {
             output_format = 3;
-        } else if (sim_scheme == "sync" || sim_scheme == "sync_multi" || sim_scheme == "rand_sync" || sim_scheme == "rand_sync_gauss") {
+        } else if (sim_scheme == "sync" || sim_scheme == "sync_multi" || sim_scheme == "rand_sync" || sim_scheme == "rand_sync_gauss" || sim_scheme == "fixed_updates") {
             output_format = 1;
         } else {
             throw invalid_argument("Invalid simulation scheme, choose: ra, round, sync, ra_multi, sync_multi, or rand_sync");
         }
     }
+
     if (output_format == 3 && (sim_scheme == "sync" || sim_scheme == "sync_multi" || sim_scheme == "rand_sync" || sim_scheme == "rand_sync_guass")) {
         throw invalid_argument("Frequency summary (output format 3) is not supported for synchronous scheme");
     }
-    if (output_format in [1, 2, 3, 7]) {
+
+    if (output_format == 1 || output_format == 2 || output_format == 3 || output_format == 7) {
+        // run simulation to get output traces
         cout << "Simulating..." << endl;
         clock_t start_time = clock();
-        setup_and_run_simulation(input_filename, output_filename, steps, runs, sim_scheme, output_format, scenarios, normalize_output, randomize_each_run, event_traces_file);
+
+        setup_and_run_simulation(input_filename, output_filename, steps, runs, sim_scheme, output_format, 
+                                 scenarios, normalize_output, randomize_each_run, event_traces_file);
+
         if (timed) {
             cout << "--- " << (clock() - start_time) / (double)CLOCKS_PER_SEC << " seconds ---" << endl;
         }
+
         if (difference) {
-            vector<string> trace_file_names;
-            for (string x : split(scenarios, ',')) {
-                trace_file_names.push_back(output_filename.substr(0, output_filename.find_last_of('.')) + "_" + x + ".txt");
+            std::vector<std::string> trace_file_names;
+            std::string base_filename = output_filename.substr(0, output_filename.find_last_of("."));
+
+            stringstream ss(scenarios);
+            string scenario;
+            while (getline(ss, scenario, ',')) {
+                std::string trace_file_name = base_filename + "_" + scenario + ".txt";
+                trace_file_names.push_back(trace_file_name);
             }
-            calculate_trace_difference(trace_file_names, output_filename.substr(0, output_filename.find_last_of('.')) + "_diff.txt");
+            
+            std::string diff_filename = base_filename + "_diff.txt";
+            calculate_trace_difference(trace_file_names, diff_filename);
         }
+
         if (concatenate) {
             if (output_format != 2) {
                 throw invalid_argument("Concatenation only supported for output format 2");
             } else {
                 vector<string> trace_file_names;
-                if (difference) {
-                    for (string x : split(scenarios, ',')[1:]) {
-                        trace_file_names.push_back(output_filename.substr(0, output_filename.find_last_of('.')) + "_diff" + x + ".txt");
+                stringstream ss(scenarios);
+                string scenario;
+                while (getline(ss, scenario, ',')) {
+                    if (difference){
+                        trace_file_names.push_back(output_filename.substr(0, output_filename.find_last_of('.')) + "_diff" + scenario + ".txt");
                     }
-                } else {
-                    for (string x : split(scenarios, ',')) {
-                        trace_file_names.push_back(output_filename.substr(0, output_filename.find_last_of('.')) + "_" + x + ".txt");
+                    else {
+                        trace_file_names.push_back(output_filename.substr(0, output_filename.find_last_of('.')) + "_" + scenario + ".txt");
                     }
                 }
+                
                 concatenate_traces(trace_file_names, output_filename.substr(0, output_filename.find_last_of('.')) + "_concat.txt");
             }
         }
-    } else if (output_format in [4, 5, 6]) {
+    } else if (output_format >= 4 && output_format <= 6) {
         if (output_format != 5) {
-            setup_and_create_truth_tables(input_filename, output_filename, scenarios);
+            setup_and_create_truth_tables(input_filename, output_filename.substr(0, output_filename.find_last_of('.')), scenarios);
         }
         if (output_format != 6) {
-            setup_and_create_rules(input_filename, output_filename, scenarios);
+            setup_and_create_rules(input_filename, output_filename.substr(0, output_filename.find_last_of('.')), scenarios);
         }
     } else {
         throw invalid_argument("Unrecognized output_format, use output_format of 1, 2, 3, 5");
